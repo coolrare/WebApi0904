5aadb3f560002b8c056a0c0f4f17a36d9606f63c
Will <doggy.huang@gmail.com>
2016-09-04 10:37:04 +0800

    05 練習對 ClientsController 套用屬性路由 ( clients , clients/{id} , clients/{id}/orders )

---
+++ WebApi0904/Controllers/ClientsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi0904.Models;
+
+namespace WebApi0904.Controllers
+{
+    public class ClientsController : ApiController
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        public ClientsController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // GET: api/Clients
+        [Route("clients")]
+        public IQueryable<Client> GetClient()
+        {
+            return db.Client;
+        }
+
+        // GET: api/Clients/5
+        [ResponseType(typeof(Client))]
+        [Route("clients/{id}")]
+        public IHttpActionResult GetClient(int id)
+        {
+            Client client = db.Client.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+
+        // GET: api/Clients/5
+        [ResponseType(typeof(Client))]
+        [Route("clients/{id}/orders")]
+        public IHttpActionResult GetClientOrders(int id)
+        {
+            var orders = db.Order.Where(p => p.ClientId == id);
+
+            return Ok(orders);
+        }
+
+        // PUT: api/Clients/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutClient(int id, Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != client.ClientId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Clients
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult PostClient(Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Client.Add(client);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = client.ClientId }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [ResponseType(typeof(Client))]
+        public IHttpActionResult DeleteClient(int id)
+        {
+            Client client = db.Client.Find(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            db.Client.Remove(client);
+            db.SaveChanges();
+
+            return Ok(client);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClientExists(int id)
+        {
+            return db.Client.Count(e => e.ClientId == id) > 0;
+        }
+    }
+}
